791491883fa0af09aaf1e221f5b82ed604e415cf
mohdabdullahmeraj <mohdabdullahmeraj1705@gmail.com>
2025-02-04 01:56:12 +0530

    Create printSubsequencesSumAnyOne.cpp

---
+++ printSubsequencesSumAnyOne.cpp
@@ -0,0 +1,34 @@
+bool printSubsequencesSumAnyOne(int index, vector<int>& ds, int s, int sum, int arr[], int n){
+    if(index == n){
+        if(s==sum){
+            for(auto it : ds){
+                cout<< it << " ";
+            }
+            cout<<endl;
+            return true;
+            
+        }
+        return false;
+    }
+    
+    ds.push_back(arr[index]);
+    s = s + arr[index];
+    if (printSubsequencesSumAnyOne(index+1, ds, s, sum, arr, n) == true) return true; 
+    s = s - arr[index];
+    ds.pop_back();
+
+    if (printSubsequencesSumAnyOne(index+1, ds, s, sum, arr, n) == true) return true;
+
+    return false;
+}
+
+int main(){
+    int arr[] = {1,2,1};
+    int n = 3;
+    int sum = 2;
+    vector<int> ds;
+    printSubsequencesSumAnyOne(0, ds, 0, sum, arr, n);
+
+    return 0;
+}     
+
